2a55274359f2b6cd2ab35266fafa4693631e1c57
Lorenzo <32592789+lrnz93@users.noreply.github.com>
2019-10-02 13:59:19 +0200

    fix list_security_rules
    function has fixed,

--- PanAPI/PANAPI.py
+++ PanAPI/PANAPI.py
@@ -1,27 +1,31 @@
 import requests
 import xml.etree.ElementTree as ET
-import simplejson as json
+import json
+import xmltodict
 
 #Static values
 APIKEY = "LUFRPT15OC9NSjRobndlMmsvT3dBazRweU9HaHNnczQ9VkhLS3oyLzFiaDdGMm9LTHU3eW0rRW9FTEh2bDhHbUpsQzg4QUlIQkdpRExUYjlTb2RDZHdoY1R6eGs5cmY0Tg=="
 VSYS = "vsys1"
 URL = "https://fwpan01.biseswar.tech/api/?type=config&action="
+ACTION = "get"
 
-def list_security_rules(url, vsys, apikey):
+def list_security_rules(url, vsys, apikey,action):
 
     xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+"'"+vsys+"'"+"]/rulebase/security"
-    r_seclist = requests.get(url+"get"+"&xpath="+xpath+"&key="+apikey)
+    r_seclist = requests.get(url+action+"&xpath="+xpath+"&key="+apikey)
+    list = []
 
     if r_seclist.status_code != 200:
         return r_seclist.status_code
 
     else:
-
+        output = []
         root_seclist = ET.fromstring(r_seclist.content)
         for child in root_seclist.iter('entry'):
-            i = (child.attrib['name'])
+            output.append(child.attrib['name'])
 
-            print i
+        klaar = json.dumps(output)
+        return klaar
 
 def edit_security_rules():
     response_code = ''
@@ -31,5 +35,5 @@ def commit_config():
     curl_response = ''
     return curl_response
 
-newlist = list_security_rules(URL, VSYS, APIKEY)
+newlist = list_security_rules(URL, VSYS, APIKEY, ACTION)
 print(newlist)
