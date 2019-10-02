009dd8df878b38e6205d70b51f4331ea03ae898b
Lorenzo <32592789+lrnz93@users.noreply.github.com>
2019-10-02 16:53:23 +0200

    fix function edit log

--- PanAPI/PANAPI.py
+++ PanAPI/PANAPI.py
@@ -1,39 +1,58 @@
 import requests
 import xml.etree.ElementTree as ET
-import json
-import xmltodict
+import sys
+
 
 #Static values
 APIKEY = "LUFRPT15OC9NSjRobndlMmsvT3dBazRweU9HaHNnczQ9VkhLS3oyLzFiaDdGMm9LTHU3eW0rRW9FTEh2bDhHbUpsQzg4QUlIQkdpRExUYjlTb2RDZHdoY1R6eGs5cmY0Tg=="
-VSYS = "vsys1"
+VSYS = "\"vsys1\""
 URL = "https://fwpan01.biseswar.tech/api/?type=config&action="
-ACTION = "get"
 
-def list_security_rules(url, vsys, apikey,action):
+def list_security_rules(url, vsys, apikey):
+    ACTION = "get"
+    xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security"
 
-    xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+"'"+vsys+"'"+"]/rulebase/security"
-    r_seclist = requests.get(url+action+"&xpath="+xpath+"&key="+apikey)
-    list = []
+    try:
+        r_seclist = requests.get(url+ACTION+"&xpath="+xpath+"&key="+apikey)
+    except requests.ConnectionError as e:
+        print e
+        sys.exit()
 
-    if r_seclist.status_code != 200:
-        return r_seclist.status_code
+    output = []
+    root_seclist = ET.fromstring(r_seclist.content)
+    for child in root_seclist.iter('entry'):
+        output.append(child.attrib['name'])
 
-    else:
-        output = []
-        root_seclist = ET.fromstring(r_seclist.content)
-        for child in root_seclist.iter('entry'):
-            output.append(child.attrib['name'])
+    #klaar = json.dumps(output)
+    return output
 
-        klaar = json.dumps(output)
-        return klaar
+def edit_security_rules(url, vsys, apikey,secrules):
 
-def edit_security_rules():
-    response_code = ''
-    return response_code
+    ACTION = "set"
+    for i in secrules:
+        xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security/rules/entry[@name=\""+i+"\"]&element=<log-setting>test_Ubuntu</log-setting>"
+        print xpath
+
+
+        try:
+            r_editrule = requests.get(url + ACTION + "&xpath=" + xpath + "&key=" + apikey)
+        except Exception:
+            print "error"
+
+    return r_editrule
+
+
+
 
 def commit_config():
     curl_response = ''
     return curl_response
 
-newlist = list_security_rules(URL, VSYS, APIKEY, ACTION)
-print(newlist)
+# Call functions
+newlist = list_security_rules(URL, VSYS, APIKEY)
+print newlist
+
+r_code = edit_security_rules(URL,VSYS,APIKEY,newlist)
+print r_code
+
+
