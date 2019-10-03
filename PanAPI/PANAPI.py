1ec56780d8c921d749e76b32a1248936bf590abd
Lorenzo <32592789+lrnz93@users.noreply.github.com>
2019-10-03 10:01:09 +0200

    add time 1 sec
    add waiting 1 sec

--- PanAPI/PANAPI.py
+++ PanAPI/PANAPI.py
@@ -1,18 +1,21 @@
 import requests
 import xml.etree.ElementTree as ET
 import sys
+import time
 
 #Static values
 APIKEY = "LUFRPT15OC9NSjRobndlMmsvT3dBazRweU9HaHNnczQ9VkhLS3oyLzFiaDdGMm9LTHU3eW0rRW9FTEh2bDhHbUpsQzg4QUlIQkdpRExUYjlTb2RDZHdoY1R6eGs5cmY0Tg=="
 VSYS = "\"vsys1\""
 URL = "https://fwpan01.biseswar.tech/api/?type=config&action="
 
+# function create firwall list
 def list_security_rules(url, vsys, apikey):
     ACTION = "get"
     xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security"
 
     try:
         r_seclist = requests.get(url+ACTION+"&xpath="+xpath+"&key="+apikey)
+        time.sleep(1)
     except requests.ConnectionError as e:
         print e
         sys.exit()
@@ -24,21 +27,24 @@ def list_security_rules(url, vsys, apikey):
 
     return r_output
 
+# function edit security rules
 def edit_security_rules(url, vsys, apikey,secrules):
 
     ACTION = "set"
     for count in secrules:
-        xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security/rules/entry[@name=\""+count+"\"]&element=<log-setting>test_Ubuntu</log-setting>"
+        xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security/rules/entry[@name=\""+count+"\"]&element=<log-setting>testing</log-setting>"
 
         try:
             r_editrule = requests.get(url + ACTION + "&xpath=" + xpath + "&key=" + apikey)
             print "rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code)
+            time.sleep(1)
         except requests.ConnectionError as e:
             print e + "rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code)
             sys.exit()
 
     return ("end edit function ")
 
+#function commit to firewall
 def commit_config(apikey):
     URL = "https://fwpan01.biseswar.tech/api/?type=commit&cmd=<commit></commit>"
 
