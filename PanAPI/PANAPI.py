978c757b6ea3a135e9936e67dd37792937b162ad
Lorenzo <32592789+lrnz93@users.noreply.github.com>
2019-10-06 14:57:20 +0200

    create variable logname

--- PanAPI/PANAPI.py
+++ PanAPI/PANAPI.py
@@ -3,10 +3,13 @@ import xml.etree.ElementTree as ET
 import sys
 import time
 
+# Lorenzo Rozenblad
+
 #Static values
 APIKEY = "LUFRPT15OC9NSjRobndlMmsvT3dBazRweU9HaHNnczQ9VkhLS3oyLzFiaDdGMm9LTHU3eW0rRW9FTEh2bDhHbUpsQzg4QUlIQkdpRExUYjlTb2RDZHdoY1R6eGs5cmY0Tg=="
 VSYS = "\"vsys1\""
 URL = "https://fwpan01.biseswar.tech/api/?type=config&action="
+LOGNAME = "test_Ubuntu"
 
 # function create firwall list
 def list_security_rules(url, vsys, apikey):
@@ -28,21 +31,21 @@ def list_security_rules(url, vsys, apikey):
     return r_output
 
 # function edit security rules
-def edit_security_rules(url, vsys, apikey,secrules):
+def edit_security_rules(url, vsys, apikey,secrules, logname):
 
     ACTION = "set"
     for count in secrules:
-        xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security/rules/entry[@name=\""+count+"\"]&element=<log-setting>testing</log-setting>"
+        xpath = "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name="+vsys+"]/rulebase/security/rules/entry[@name=\""+count+"\"]&element=<log-setting>"+logname+"</log-setting>"
 
         try:
             r_editrule = requests.get(url + ACTION + "&xpath=" + xpath + "&key=" + apikey)
-            print ("rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code))
+            print("rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code))
             time.sleep(1)
         except requests.ConnectionError as e:
-            print (e + "rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code))
+            print(e + "rule: "+count+"---"+ "statuscode: "+str(r_editrule.status_code))
             sys.exit()
 
-    return ("end edit function ")
+    return("End edit function ")
 
 #function commit to firewall
 def commit_config(apikey):
@@ -50,20 +53,20 @@ def commit_config(apikey):
 
     try:
         r_commit = requests.get(URL + "&key=" + apikey)
-        print ("Commit succesfull")
+        print("Commit succesfull")
 
     except requests.ConnectionError as e:
         print (e)
         sys.exit()
 
-    return ("end commit function ")
+    return("End commit function ")
 
 # Call functions
 newlist = list_security_rules(URL, VSYS, APIKEY)
-print (newlist)
+print(newlist)
 
-r_code = edit_security_rules(URL,VSYS,APIKEY,newlist)
-print (r_code)
+r_code = edit_security_rules(URL,VSYS,APIKEY,newlist, LOGNAME)
 
-r_commit = commit_config(APIKEY)
-print (r_commit)
+print("Check before commit!")
+#r_commit = commit_config(APIKEY)
+#print (r_commit)
